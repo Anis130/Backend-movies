c48e368c8842858dc5ed6ff725dee116d906aa5d
Anis Bellil <anissbellil13@gmail.com>
2024-10-20 11:33:25 +0200

    aa

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const dbConnect = () => {
   try {
-    mongoose.connect("mongodb://localhost:27017/movies")
+    mongoose.connect("mongodb+srv://anissbellil13:toNS9INDk3j0BPJm@cluster0.groyl.mongodb.net/movies")
     console.log("connected to database")
   } catch (error) {
     console.log("error connecting to database")
